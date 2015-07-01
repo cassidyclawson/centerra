a7df1513e9202c00760c35aac08d6b1bfe2e0cce
Cassidy Clawson <cassidyclawson@gmail.com>
2015-07-01 00:29:42 -0700

    snapshot of progress on nav

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,2 +1,5 @@
+//= require typekit
 //= require jquery-1.11.3
+//= require fastclick
+//= require meanmenu
 //= require centerra
--- source/javascripts/centerra.js
+++ source/javascripts/centerra.js
@@ -1,1 +1,8 @@
-console.log('hello')
+jQuery(document).ready(function () {
+
+    // Instantiate Fastclick for better mobile performance
+    FastClick.attach(document.body);
+
+    // Instantiate Meanmenu for mobile navigation
+    jQuery('.nav-primary').meanmenu({ meanScreenWidth: "640" });
+});
