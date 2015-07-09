ced5c57cdd7d3de9387933203417aa84606b9af8
Alok Jain <alokjain@xhtml-lab.com>
2015-07-09 21:35:09 +0530

    created HTML partials in gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,46 @@
 var gulp = require('gulp'),
+    autoprefixer = require('gulp-autoprefixer'),
     connect = require('gulp-connect'),
-    gutil = require('gulp-util'),
+    fileinclude = require('gulp-file-include'),
+    rename = require('gulp-rename'),
     sass = require('gulp-ruby-sass'),
     sourcemaps = require('gulp-sourcemaps'),
-    autoprefixer = require('gulp-autoprefixer');
+    gutil = require('gulp-util');
 
 //Paths
-var scssSource = 'components/stylesheets/',
-    cssDestination = 'source/css/';
+var paths = {
+    html_tempaltes: 'source/templates/',
+    scssSource: 'source/stylesheets/',
+    cssDestination: 'build/css/'
+}
+
+gulp.task('fileinclude', function() {
+    return gulp.src(paths.html_tempaltes + '*.tpl.html')
+    .pipe(fileinclude())
+    .pipe(rename({
+      extname: ""
+    }))
+    .pipe(rename({
+      extname: ".html"
+    }))
+    .pipe(gulp.dest('build'));
+});
 
 //Task: Sass
 gulp.task('sass', function () {
-    return sass(scssSource, {sourcemap: true})
+    return sass(paths.scssSource, {sourcemap: true})
         .on('error', function (err) {
             gutil.log('Error!', err.message);
         })
         .pipe(autoprefixer('last 2 versions'))
         .pipe(sourcemaps.write('../maps'))
-        .pipe(gulp.dest(cssDestination));
+        .pipe(gulp.dest(paths.cssDestination));
 });
 
 //Task: webserver
 gulp.task('webserver', function() {
   connect.server({
-    root: 'source',
+    root: 'build',
     livereload: true,
     port: 4567
   });
@@ -31,8 +48,8 @@ gulp.task('webserver', function() {
 
 //Default task and watch expression
 gulp.task('watch', function() {
-  gulp.watch(scssSource + '*.scss', ['sass']);
-    gulp.watch(scssSource + 'partials/*.scss', ['sass']);
+    gulp.watch(paths.scssSource + '*.scss', ['sass']);
+    gulp.watch(paths.scssSource + 'partials/*.scss', ['sass']);
 })
 
 gulp.task('default', ['webserver', 'sass', 'watch']);
